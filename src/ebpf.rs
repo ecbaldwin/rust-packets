717c5fa38d990704c973a8b3dbd7912f8f8b794a
Carl Baldwin <carl@ecbaldwin.net>
2024-07-07 21:18:51 -0600

    tc

--- src/ebpf.rs
+++ src/ebpf.rs
@@ -1,10 +1,20 @@
-use aya_ebpf::programs::XdpContext;
+use aya_ebpf::programs::{TcContext, XdpContext};
 
 pub trait HasRange<T>: Sized {
     fn range(&self) -> core::ops::Range<T>;
 }
 
 impl HasRange<*const core::ffi::c_void> for &XdpContext {
+    #[inline(always)]
+    fn range(&self) -> core::ops::Range<*const core::ffi::c_void> {
+        return core::ops::Range {
+            start: self.data() as *const core::ffi::c_void,
+            end: self.data_end() as *const core::ffi::c_void,
+        };
+    }
+}
+
+impl HasRange<*const core::ffi::c_void> for &TcContext {
     #[inline(always)]
     fn range(&self) -> core::ops::Range<*const core::ffi::c_void> {
         return core::ops::Range {
